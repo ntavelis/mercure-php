c92702a87d798523e1fa82800e3571fc64b7380d
Athanasios Ntavelis <davelis89@gmail.com>
2021-11-02 10:20:27 +0200

    bumping up dependencies

--- src/Providers/SubscriberTokenProvider.php
+++ src/Providers/SubscriberTokenProvider.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ntavelis\Mercure\Providers;
 
-use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Key\InMemory;
 use Ntavelis\Mercure\Contracts\TokenProviderInterface;
 
 class SubscriberTokenProvider implements TokenProviderInterface
@@ -20,8 +20,9 @@ class SubscriberTokenProvider implements TokenProviderInterface
 
     public function getToken(array $tokenData): string
     {
-        return (string)(new Builder())
+        $configuration = Configuration::forSymmetricSigner(new Sha256(), InMemory::plainText($this->token));
+        return $configuration->builder()
             ->withClaim('mercure', ['subscribe' => $tokenData])
-            ->getToken(new Sha256(), new Key($this->token));
+            ->getToken($configuration->signer(), $configuration->signingKey())->toString();
     }
 }
